c89f541a6f7f14ddaffab8279b9f8f640432517d
Harry <hjwp2@cantab.net>
2016-06-04 11:44:00 +0100

    fixup email send view use get request

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def send_login_email(request):
     email = request.POST['email']
     uid = str(uuid.uuid4())
     Token.objects.create(email=email, uid=uid)
-    url = 'http://localhost/accounts/login/{uid}/'.format(uid=uid)
+    url = 'http://localhost/accounts/login/?uid={uid}/'.format(uid=uid)
     send_mail(
         'Your login link for Superlists',
         'Use this link to log into the site:\n\n {url}\n'.format(url=url),
